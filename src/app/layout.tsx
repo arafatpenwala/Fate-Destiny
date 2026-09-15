cb34775ee24507ee2d52e41c24397f9cfecb732f
Arafat Penwala <arafatpenwala@gmail.com>
2026-09-16 03:32:04 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,18 +50,6 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  icons: {
-    icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
-      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/favicon-512x512.png', sizes: '512x512', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
-  },
 };
 
 import SmoothScroll from "@/components/SmoothScroll";
